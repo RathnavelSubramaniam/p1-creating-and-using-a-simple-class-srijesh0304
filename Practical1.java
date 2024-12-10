84d6fa0beab127b280ad0c971c29485d9c02bd60
SRIJESH S <srijesh0304@gmail.com>
2024-12-10 08:32:48 +0000

    1

--- Practical1.java
+++ Practical1.java
@@ -27,8 +27,8 @@ class Practical1
     public static void main(String[] args)
     {
         // Create two Person objects
-        Person person1 =new Person("John", "Doe", 30);
-        Person person2 =new Person("Alice", "Smith",25);
+        Person person1 =new Person("John", " Doe", 30);
+        Person person2 =new Person("Alice", " Smith",25);
         System.out.println("Person1: " + person1.getFullName());
         System.out.println("Person2: " + person2.getFullName());
         double averageAge=(person1.age+ person2.age)/2.0;
